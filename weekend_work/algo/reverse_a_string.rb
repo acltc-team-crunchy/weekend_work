e4e0416ea054a487d19217d894646b88da706d79
Mark Richardson <markeban@yahoo.com>
2016-04-25 18:56:21 -0700

    just finished weekend 2

---
+++ weekend_work/algo/reverse_a_string.rb
@@ -0,0 +1,28 @@
+# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# returns the reverse. The one caveat: Don't use the reverse method that already
+# comes with Ruby!
+
+def reverse_a_string(string)
+  word = ""
+  characters = string.split(//)
+  characters.size.times  do
+    word << characters.pop
+  end
+  return word
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING reverse_a_string..."
+puts
+
+result = reverse_a_string("abcde")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "edcba"
+  puts "PASS!"
+else
+  puts "F"
+end
